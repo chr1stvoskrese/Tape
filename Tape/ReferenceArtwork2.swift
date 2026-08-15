064fea73c607ba75a9987a9fbb307c3bc65939be
chr1stvoskrese <c826749264@icloud.com>
2026-08-15 17:24:47 +0300

    Delete obsolete artwork compatibility source

--- Tape/ReferenceArtwork2.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-/// Compatibility name for the physical-player renderer.
-/// Keep the reference image in a single, known-good implementation.
-enum ReferenceArtwork2 {
-    static var image: UIImage {
-        ReferenceArtwork.image
-    }
-}
